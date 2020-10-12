36638d614246834e8cccca5cfda6700ded7c4701
Joe Warren <joe.warren7@protonmail.com>
2020-10-11 23:04:53 -0500

    feat: add dark mode button with state and footer component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Footer from './components/footer/footer';
 import './App.css';
 
 import ArticlesFeed from './components/articles-feed/articles-feed'
 
 const App = () => {
+  const [mode, setMode] = useState(localStorage.getItem('cachedMode') || '');
+  const modeText = mode === 'light' ? 'dark' : 'light';
+
+  useEffect(() => {
+    localStorage.setItem('cachedMode', mode)
+  }, [mode])
+
+  const toggleMode = () => mode === 'light' ? setMode('dark') : setMode('light');
+
   return (
-    <div className="container">
-      <h1 className="title"> Black Tech Daily </h1>
-      <h2 className="subtitle"> Your Daily Source for News on Blacks in IT </h2>
-      <ArticlesFeed/>
+    <div className={`container ${mode}-container`}>
+      <div className={`content ${mode}-content`}>
+        <h1 className="header"> Black Tech Daily </h1>
+        <button onClick={toggleMode}>{modeText} mode</button>
+        <h2 className="subheader"> Your Daily Source for News on Blacks in IT </h2>
+        <ArticlesFeed />
+      </div>
+      <Footer />
     </div>
   );
 }
