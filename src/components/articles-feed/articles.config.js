67f1544d13206600595e881a6848aa734f07c950
Joe Warren <joe.warren7@protonmail.com>
2020-10-16 23:25:27 -0500

    fix: use moment to get articles from last 29 days and sort by date

---
+++ src/components/articles-feed/articles.config.js
@@ -0,0 +1,12 @@
+import { apiKey } from '../../env';
+import moment from 'moment';
+
+const daysAgo = `from=${moment().subtract(29, 'days').calendar()}&`
+
+export const blackTechArticles =
+  'http://newsapi.org/v2/everything?' +
+  'qinTitle="black tech" OR "blacktech" OR "blacktechtwitter" OR "black tech twitter" OR "blacks in tech" OR "black engineers" OR "black devs" OR "black developers" OR "black stem" OR "black ceo" OR "black cto" OR "african american ceo" OR "black-owned" OR "black engineering" OR "spend black" OR "shop black" OR "mogul millenial" OR "pushblack" OR "black tech pipeline" OR "blacktechpipeline" OR "ObsidianTech" OR "black tech conference" OR "black tech start-up" OR "black tech entrepreneur" OR "black-owned business"&' +
+  daysAgo +
+  'sortBy=publishedAt&' +
+  'language=en&' +
+  apiKey;
