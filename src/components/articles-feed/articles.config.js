86b062c03bac9c121b19b23aeddef614185e2781
Joe Warren <joe.warren7@protonmail.com>
2020-10-24 11:58:11 -0500

    fix: remove duplicate params

--- src/components/articles-feed/articles.config.js
+++ src/components/articles-feed/articles.config.js
@@ -1,7 +1,7 @@
 const param = {
   country: 'country=us',
   lang: 'lang=en',
-  query: `https://gnews.io/api/v4/search?q="black engineer" OR "black developer" OR "black ceo" OR "black-owned" OR "black engineer" OR "spend black" OR "shop black" OR "black start-up" OR "black entrepreneur" OR "black excellence"&token=593f009a6b270061a6eab180bb3e5f3a&lang=en`, // 200 char limit
+  query: `https://gnews.io/api/v4/search?q="black engineer" OR "black developer" OR "black ceo" OR "black-owned" OR "black engineer" OR "spend black" OR "shop black" OR "black start-up" OR "black entrepreneur" OR "black excellence"`, // 200 char limit
   sorted: 'sortBy=publishedAt',
   token: 'token=593f009a6b270061a6eab180bb3e5f3a',
 }
