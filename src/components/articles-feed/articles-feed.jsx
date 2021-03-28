8969ed1a04569196a9a8df6879dfbd954e876003
Joe Warren <joe.warren7@protonmail.com>
2021-03-28 14:25:19 -0500

    feat: setup error handling for failed api call

--- src/components/articles-feed/articles-feed.jsx
+++ src/components/articles-feed/articles-feed.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import moment from 'moment';
+import ErrorModal from '../error-modal/error-modal';
 import { articlesQuery } from './articles.config';
 import './articles-feed.css';
 import { isDev } from '../../globals';
 
 const ArticleFeed = props => {
   const [articles, setArticles] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  const getArticles = () => {
+    setTimeout(() => {
+      axios
+        .get(articlesQuery)
+        .then(res => {
+          setArticles(isDev ? res.data : res.data.articles);
+        })
+        .catch(err => {
+          console.error(err);
+          if (err) {
+            setIsError(true);
+          }
+        });
+    }, 1000);
+  };
 
   useEffect(() => {
-    setTimeout(() => {
-      axios.get(articlesQuery).then(res => {
-        console.log(res.status);
-        setArticles(isDev ? res.data : res.data.articles);
-      });
-    }, 1000);
+    getArticles();
   }, []);
 
   const uniqueArticles = Array.from(
@@ -23,28 +36,38 @@ const ArticleFeed = props => {
     return articles.find(article => article.title === title);
   });
 
-  return articles.length > 0
-    ? uniqueArticles.map((article, index) => {
-        return (
-          <p className='article' key={index}>
-            <span className={`article-header-${props.mode}`}>
-              Published{' '}
-              {article.source.name ? `by ${article.source.name}` : null}{' '}
-              {moment(article.publishedAt).fromNow()}
-            </span>
-            <br />
-            <a
-              className={`article-title-${props.mode}`}
-              href={article.url}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {article.title}
-            </a>
-          </p>
-        );
-      })
-    : 'Loading...';
+  const handleClose = () => {
+    setIsError(false);
+    getArticles();
+  };
+
+  return (
+    <>
+      {articles.length > 0
+        ? uniqueArticles.map((article, index) => {
+            return (
+              <p className='article' key={index}>
+                <span className={`article-header-${props.mode}`}>
+                  Published{' '}
+                  {article.source.name ? `by ${article.source.name}` : null}{' '}
+                  {moment(article.publishedAt).fromNow()}
+                </span>
+                <br />
+                <a
+                  className={`article-title-${props.mode}`}
+                  href={article.url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {article.title}
+                </a>
+              </p>
+            );
+          })
+        : 'Loading...'}
+      {isError && <ErrorModal onClose={handleClose} />}
+    </>
+  );
 };
 
 export default ArticleFeed;
--- src/components/articles-feed/articles.config.js
+++ src/components/articles-feed/articles.config.js
@@ -1,9 +1,13 @@
 import { isDev } from '../../globals';
 
+const query = `"black engineer" OR "black founders" OR "buy black" OR "black ceo" OR "black-owned" OR "hbcu" OR "spend black" OR "shop black" OR "black startup" OR "black entrepreneur" OR "black excellence"`;
+
+// console.log('query.length:', query.length);
+
 const param = {
   country: 'country=us',
   lang: 'lang=en',
-  query: `https://gnews.io/api/v4/search?q="black engineer" OR "blacklivesmatter" OR "blm" OR "black ceo" OR "black-owned" OR "hbcu" OR "spend black" OR "shop black" OR "black start-up" OR "black entrepreneur" OR "black excellence"`, // 200 char limit
+  query: `https://gnews.io/api/v4/search?q=${query}`, // 200 char limit
   sorted: 'sortBy=publishedAt',
   token: 'token=593f009a6b270061a6eab180bb3e5f3a',
 };
