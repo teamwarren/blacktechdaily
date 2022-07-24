73500c7a90f85598bbbf86bba529c9ff34ab391d
Joe Warren <joe.warren7@protonmail.com>
2022-07-24 16:36:45 -0500

    fix: update footer styling

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -4,5 +4,11 @@ import Footer from './footer';
 
 test('renders the footer', () => {
   const { getByText } = render(<Footer />);
-  expect(getByText(/© 2020 | TeamWarren/i)).toBeInTheDocument();
+  expect(getByText(/Copyright ©/i)).toBeInTheDocument();
+  expect(getByText(/Black Tech Daily/i)).toBeInTheDocument();
+  expect(getByText(/2022/i)).toBeInTheDocument();
+  expect(getByText(/Black Tech Daily/i).closest('a')).toHaveAttribute(
+    'href',
+    'https://github.com/teamwarren/blacktechdaily'
+  );
 });
